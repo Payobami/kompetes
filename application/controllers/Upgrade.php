6bce666e4b829d9e809856c17b9519e9277de51b
Prudentay <prudentprogrammer@gmail.com>
2018-05-04 10:10:09 +0100

    removing checkout view

--- application/controllers/Upgrade.php
+++ application/controllers/Upgrade.php
@@ -72,7 +72,7 @@ class Upgrade extends CI_Controller{
 
                 if($this->form_validation->run() == false) {
                     $this->load->view("template/header", $data);
-                    $this->load->view("checkout", $data);
+                    //$this->load->view("checkout", $data);
                     $this->load->view("template/footer", $data);
                 }
 
